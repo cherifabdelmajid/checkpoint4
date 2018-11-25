7e2eee99e18fa04322d51e3d3da715d846ae02b5
cherifabdelmajid <44751436+cherifabdelmajid@users.noreply.github.com>
2018-11-25 17:15:56 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var test = 10; //wala 3andi variables test valuer 10;
+test = 121; // test walet 121
+test = test + 10 // test walet 131
+test = test - 10
+test = test * 10
+test = test / 10 
+test = test % 10
+
+var alpha = 4
+
+test = test + alpha // 131 + 4 = 135
+function yo(){
+    alert('yo');
+}
+
+
+setTimeout(yo,2000)
+
+alert("ahla bik")
+var name = prompt("what's your name?")
+alert('marihba bik' + name );
+alert(confirm("can i have your name?"));
+var element = document.querySelector('h1');
+element.style.backgroundColor = "red";
+element.style.fontsize="40px";
+var element = document.querySelector('#hedha').setAttribute("id","http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Pomeranian-1.jpg")
+document.getElemementbyid("hello").innerHTML ="just lose it";
+
+function Test(){
+    i=0;
+    setInterval(function(){
+        alert(i);
+        i=i+1
+    },2000);
+}
